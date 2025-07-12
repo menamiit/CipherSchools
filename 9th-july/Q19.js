da2b7f3f299ff6fab8f20ff9684b732711189c13
Namit <pareeknamit8@gmail.com>
2025-07-12 18:53:58 +0530

    Commited some changes and added a few files

---
+++ 9th-july/Q19.js
@@ -0,0 +1,24 @@
+// 1. Show a Greeting Using alert()
+alert('Hello, welcome to my page!');
+
+// 2. Test the Function with console.log()
+console.log(sum(2, 3));
+console.log(sum(10, 25));
+console.log(sum(-1, 8));
+
+// 3. Test the Function with console.log()
+const sum = (a, b) => a + b;
+
+// 4. Logging Variable Types with typeof
+let x = 42;
+console.log(typeof x);
+
+let name = "Alice";
+console.log(typeof name);
+
+let isActive = true;
+console.log(typeof isActive);
+
+
+// 5. Logging window
+console.log(window);
